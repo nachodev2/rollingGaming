9261e4c05b50be91182900cf7f40f6da78da0f03
Luciana Ruiz <lucianaruiz.934@gmail.com>
2025-07-19 01:06:53 -0300

    Agregué nuestra paleta de colores e importé una fuente para el título de la card

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import FilaProducto from "./components/pages/producto/FilaProducto"
 
 function App() {
--- src/components/pages/producto/CardProducto.jsx
+++ src/components/pages/producto/CardProducto.jsx
@@ -1,20 +1,22 @@
-import {Card , ListGroup} from 'react-bootstrap';
+import { Card, ListGroup } from 'react-bootstrap';
 
 function CardProducto() {
   return (
-    <Card className='p-0 rounded-4 overflow-hidden'>
+    <Card className='p-0 rounded-4 overflow-hidden bg-card'>
       <Card.Img className='img-card' variant="top" src="https://wallup.net/wp-content/uploads/2017/03/27/420785-League_of_Legends-Ezreal.jpg" />
       <Card.Body>
-        <Card.Title className='text-danger'>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
+        <Card.Title className='titulo-card text-light'>League of Legends</Card.Title>
+        <div>
+          <i class="bi bi-star-fill star-color"></i>
+          <i class="bi bi-star-fill star-color"></i>
+          <i class="bi bi-star-fill star-color"></i>
+          <i class="bi bi-star-fill star-color"></i>
+          <i class="bi bi-star-fill star-color"></i>
+        </div>
       </Card.Body>
       <ListGroup className="list-group-flush">
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+        <ListGroup.Item className='bg-card'>Cras justo odio</ListGroup.Item>
+        <ListGroup.Item className='bg-card'>Dapibus ac facilisis in</ListGroup.Item>
       </ListGroup>
       <Card.Body>
         <Card.Link href="#">Card Link</Card.Link>
