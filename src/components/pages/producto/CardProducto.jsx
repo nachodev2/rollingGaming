6a0dba077826ed031ce9402cdaaf2e59428ca2b3
Luciana Ruiz <lucianaruiz.934@gmail.com>
2025-07-19 02:14:42 -0300

    Agregué hover al botón y la card

--- src/components/pages/producto/CardProducto.jsx
+++ src/components/pages/producto/CardProducto.jsx
@@ -2,7 +2,7 @@ import { Card, Button } from 'react-bootstrap';
 
 function CardProducto() {
   return (
-    <Card className='p-0 rounded-4 overflow-hidden cardProducto card'>
+    <Card className='p-0 rounded-4 overflow-hidden cardProducto card me-3 mb-3'>
       <Card.Img className='img-card' variant="top" src="https://wallup.net/wp-content/uploads/2017/03/27/420785-League_of_Legends-Ezreal.jpg" />
       <Card.Body className='bg-card'>
         <Card.Title className='titulo-card text-light'>League of Legends</Card.Title>
@@ -13,16 +13,18 @@ function CardProducto() {
           <i class="bi bi-star-fill color-rosa me-1"></i>
           <i class="bi bi-star-fill color-rosa me-1"></i>
         </Card.Text>
-        <Card.Text className='text-light'>
+        <Card.Text className='text-light p-0'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit omnis.
         </Card.Text>
         <Card.Text className='color-rosa fw-bold fs-4'>
           $59.99
         </Card.Text>
+        <div className="d-flex justify-content-center">
+          <Button className='rounded-5 fw-bold py-2 btn-card'>Comprar ahora <i class="bi bi-cart3 ms-2"></i></Button>
+        </div>
       </Card.Body>
-      <Card.Body className='bg-card d-flex justify-content-center'>
-        <Button className='rounded-5 fw-bold py-2 btn-card'>Comprar ahora <i class="bi bi-cart3 ms-2"></i></Button>
-      </Card.Body>
+
+
     </Card>
   );
 }
--- src/components/pages/producto/FilaProducto.jsx
+++ src/components/pages/producto/FilaProducto.jsx
@@ -3,7 +3,10 @@ import CardProducto from "./CardProducto"
 
 const FilaProducto = () => {
   return (
-    <Row xs={1} md={3} lg={4} className="bg-danger p-5 contenedor-card">
+    <Row xs={1} md={3} lg={4} className="bg-dark p-5 contenedor-card">
+      <CardProducto></CardProducto>
+      <CardProducto></CardProducto>
+      <CardProducto></CardProducto>
       <CardProducto></CardProducto>
     </Row>
   )
