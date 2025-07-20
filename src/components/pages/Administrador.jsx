d55309ae82b1599d290c13322d5bc22118e799df
Claudia Ximena Barrientos <cxbarrientos1@gmail.com>
2025-07-20 17:15:41 -0300

    agregue seccion de cards de juegos usuarios y categorias en page Admin

--- src/components/pages/Administrador.jsx
+++ src/components/pages/Administrador.jsx
@@ -1,9 +1,35 @@
-import { Table } from "react-bootstrap";
+import { Table, Container, Card, Row, Col } from "react-bootstrap";
 
 const Administrador = () => {
   return (
-    <div>
-      <Table striped bordered hover variant="dark">
+    <Container>
+      <section className="mt-3 mb-3 text-center">
+        <Row className="justify-content-center">
+          <Col xs={4}>
+            <Card border="primary">
+              <Card.Body>
+                <Card.Text>Juegos: 10</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs={4}>
+            <Card border="secondary">
+              <Card.Body>
+                <Card.Text>Usuarios: 2</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs={4}>
+            <Card border="info">
+              <Card.Body>
+                <Card.Text>Categorias: 5</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          
+        </Row>
+      </section>
+      <Table striped bordered hover responsive variant="dark">
         <thead>
           <tr>
             <th>Imagen</th>
@@ -36,7 +62,7 @@ const Administrador = () => {
           </tr>
         </tbody>
       </Table>
-    </div>
+    </Container>
   );
 };
 
