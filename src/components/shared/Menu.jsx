b9f58fc19147a3e219e21c713320f101a8072542
Jose Romero <kira1698@gmail.com>
2025-07-20 16:21:48 -0300

    se cambiaron los titulos del maquetado inicial y se agrego el logo con su respectivo estilo

--- src/components/shared/Menu.jsx
+++ src/components/shared/Menu.jsx
@@ -1,39 +1,49 @@
-import {Container, Nav, Navbar, NavDropdown} from 'react-bootstrap'
-
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import logo from "../imgNavbarFooter/Logotipo-rollinggames.png";
 const Menu = () => {
   return (
     <div>
       <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+        <div className="d-flex justify-content-center w-25">
+        <img src={logo} alt="Logo de RollingGames" className="logoNav"/>
+        </div>
+        <Container >
+          <Navbar.Brand href="#home" className="text-start">
+            Rolling Games
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#features">Tienda</Nav.Link>
+              <Nav.Link href="#features">Favoritos</Nav.Link>
+              <Nav.Link href="#features">Contacto</Nav.Link>
+              <Nav.Link href="#pricing">Sobre nosotros</Nav.Link>
+              <NavDropdown
+                title="Categoria de Juegos"
+                id="collapsible-nav-dropdown"
+              >
+                <NavDropdown.Item href="#action/3.1">Accion</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Aventura</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Estrategia
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Mostrar más
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">Iniciar Sesion</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                Crear Cuenta
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
