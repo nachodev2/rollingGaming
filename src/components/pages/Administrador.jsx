a0cb5a2df4436103222f8892f99b3711f768efe9
Claudia Ximena Barrientos <cxbarrientos1@gmail.com>
2025-07-20 17:40:05 -0300

    cree el componente FilaProducto y lo agregue en el componente Admin

--- src/components/pages/Administrador.jsx
+++ src/components/pages/Administrador.jsx
@@ -1,4 +1,5 @@
 import { Table, Container, Card, Row, Col, Button } from "react-bootstrap";
+import FilaProducto from "./producto/FilaProducto";
 
 const Administrador = () => {
   return (
@@ -51,26 +52,9 @@ const Administrador = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td colSpan={2}>Larry the Bird</td>
-            <td>@twitter</td>
-            <td>@mdo</td>
-          </tr>
+          <FilaProducto></FilaProducto>
+          <FilaProducto></FilaProducto>
+          <FilaProducto></FilaProducto>
         </tbody>
       </Table>
     </Container>
--- src/components/pages/producto/FilaProducto.jsx
+++ src/components/pages/producto/FilaProducto.jsx
@@ -1,10 +1,22 @@
+import { Button } from "react-bootstrap"
 
 
 const FilaProducto = () => {
   return (
-    <div>
-      
-    </div>
+    <tr>
+      <td>imagen</td>
+      <td>nombre juego</td>
+      <td>categoria</td>
+      <td>precio</td>
+      <td className="">
+        <Button className="me-lg-2 btn btn-warning">
+          <i className="bi bi-pencil-square"></i>
+        </Button>
+        <Button variant="danger">
+          <i className="bi bi-trash"></i>
+        </Button>
+      </td>
+    </tr>
   )
 }
 
