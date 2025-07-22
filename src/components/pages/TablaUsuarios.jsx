09acea218bb9fb87b96e60163f649e05bf1a5c92
Claudia Ximena Barrientos <cxbarrientos1@gmail.com>
2025-07-22 20:46:23 -0300

    cree nuevos componentes TablaJuegos y TablaUsuarios para el control del Admin

---
+++ src/components/pages/TablaUsuarios.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const TablaUsuarios = () => {
+    return (
+        <>
+      <div className="d-flex justify-content-between align-items-center mt-5">
+        <h1 className="fs-4">Tabla de Juegos</h1>
+        <div>
+          <Button className="btn btn-primary me-2">
+            <i className="bi bi-file-earmark-plus"></i>
+          </Button>
+          <Button variant="info">
+            <i className="bi bi-database-fill-up"></i>
+          </Button>
+        </div>
+      </div>
+      <hr />
+      <Table striped bordered hover responsive variant="dark">
+        <thead>
+          <tr>
+            <th>Imagen</th>
+            <th>Nombre</th>
+            <th>Categoria</th>
+            <th>Precio</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          <FilaProductoTabla></FilaProductoTabla>
+          <FilaProductoTabla></FilaProductoTabla>
+          <FilaProductoTabla></FilaProductoTabla>
+        </tbody>
+      </Table>
+    </>
+    );
+};
+
+export default TablaUsuarios;
