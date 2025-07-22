8a71baff01451bc123e856967d819af3f32637c7
Alessandra Borges <aleborges110900@gmail.com>
2025-07-22 00:09:52 -0300

    He agregado media queries para la pagina de error404.

--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -1,16 +1,17 @@
-
+import '/src/Error404.css';
 
 const Error404 = () => {
   return (
-    <div >
+    <div className="error-container">
       <h1>Página no encontrada</h1>
       <img 
         src="/error404-2.png" 
         alt="Error 404 - Página no encontrada"
+        className="error-image"
       />
       <p>Lo sentimos, la página que buscás no existe.</p>
     </div>
-  )
-}
+  );
+};
 
-export default Error404
+export default Error404;
