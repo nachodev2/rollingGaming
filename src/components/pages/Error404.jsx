ab010db10907f413cc2e073056fe84bf1de12589
Alessandra Borges <aleborges110900@gmail.com>
2025-07-21 23:24:18 -0300

    Agrego texto e imagen a la página de error404

--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -2,8 +2,13 @@
 
 const Error404 = () => {
   return (
-    <div>
-      
+    <div >
+      <h1>Página no encontrada</h1>
+      <img 
+        src="/error404-2.png" 
+        alt="Error 404 - Página no encontrada"
+      />
+      <p>Lo sentimos, la página que buscás no existe.</p>
     </div>
   )
 }
