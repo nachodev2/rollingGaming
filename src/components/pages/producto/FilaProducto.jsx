0fb35770481c13ef90ed70390e94ef1a6db9d458
Luciana Ruiz <lucianaruiz.934@gmail.com>
2025-07-23 00:23:08 -0300

    Modifiqué la card de juegos para hacer la card de categorías

--- src/components/pages/producto/FilaProducto.jsx
+++ src/components/pages/producto/FilaProducto.jsx
@@ -1,13 +1,18 @@
 import { Row, Col } from "react-bootstrap"
 import CardProducto from "./CardProducto";
+import CardCategorias from "./CardCategorias";
 
 const FilaProducto = () => {
   return (
     <Row xs={1} md={3} lg={4} className="p-5 contenedor-card g-4">
+      {/* <Col><CardProducto /></Col>
       <Col><CardProducto /></Col>
       <Col><CardProducto /></Col>
-      <Col><CardProducto /></Col>
-      <Col><CardProducto /></Col>
+      <Col><CardProducto /></Col> */}
+      <CardCategorias></CardCategorias>
+      <CardCategorias></CardCategorias>
+      <CardCategorias></CardCategorias>
+      <CardCategorias></CardCategorias>
     </Row>
   )
 }
