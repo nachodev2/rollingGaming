310dd50b65528140526270e21cac2857acfa4068
Luciana Ruiz <lucianaruiz.934@gmail.com>
2025-07-23 09:50:01 -0300

    Verifiqué que sigan estando bien las cards después de borrar clases que no estaba utilizando

--- src/components/pages/producto/CardProducto.jsx
+++ src/components/pages/producto/CardProducto.jsx
@@ -4,8 +4,8 @@ const CardProducto = () => {
   return (
     <div className="contenedor-card">
       <Card className='p-0 rounded-4 overflow-hidden card border-secondary'>
-        <Card.Img className='img-card' variant="top" src="https://wallup.net/wp-content/uploads/2017/03/27/420785-League_of_Legends-Ezreal.jpg" />
-        <Card.Body className='bg-card'>
+        <Card.Img variant="top" src="https://wallup.net/wp-content/uploads/2017/03/27/420785-League_of_Legends-Ezreal.jpg" />
+        <Card.Body>
           <Card.Title className='titulo-card text-light'>League of Legends</Card.Title>
           <Card.Text>
             <i className="bi bi-star-fill me-1 text-secondary"></i>
@@ -14,7 +14,7 @@ const CardProducto = () => {
             <i className="bi bi-star-fill me-1 text-secondary"></i>
             <i className="bi bi-star-fill me-1 text-secondary"></i>
           </Card.Text>
-          <Card.Text className='text-color p-0'>
+          <Card.Text className='p-0'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit omnis.
           </Card.Text>
           <Card.Text className='text-secondary fw-bold fs-4'>
