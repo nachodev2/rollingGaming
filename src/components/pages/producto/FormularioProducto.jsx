39505a41577a756692e58eb3581183bbaec95422
Claudia Ximena Barrientos <cxbarrientos1@gmail.com>
2025-07-23 11:56:27 -0300

    agregue un formulario de react boostrap para el formulario de añadir juegos

--- src/components/pages/producto/FormularioProducto.jsx
+++ src/components/pages/producto/FormularioProducto.jsx
@@ -1,9 +1,31 @@
+import { Button, Container, Form } from "react-bootstrap";
 const FormularioProducto = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Container className="mt-3">
+      <h1>Añadir un juego</h1>
+      <hr />
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-danger">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
 
-export default FormularioProducto
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
+  );
+};
+
+export default FormularioProducto;
