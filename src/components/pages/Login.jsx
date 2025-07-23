3dcc434c674aaf1a58fc3d8b105f0bfe54befbb9
JoseBaza <musulmanco@gmail.com>
2025-07-22 23:50:15 -0300

    se crea la estructura del login modal

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,11 +1,60 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
 
+function Login() {
+  const [show, setShow] = useState(false);
 
-const Login = () => {
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Iniciar Sesión
+      </Button>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Iniciar Sesión</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Group className="mb-3" controlId="formCorreo">
+            <Form.Label>Correo Electrónico</Form.Label>
+            <Form.Control type="email" placeholder="correo@rollinggaming.com" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formContrasenia">
+            <Form.Label>Contraseña</Form.Label>
+            <Form.Control type="password" />
+          </Form.Group>
+
+          <Form>
+            {["checkbox"].map((type) => (
+              <div key={`default-${type}`} className="mb-3">
+                <Form.Check
+                  type={type}
+                  id={`default-${type}`}
+                  label={`Recordarme`}
+                />
+              </div>
+            ))}
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cerrar
+          </Button>
+          <Button variant="light">Enviar</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
 
-export default Login
+export default Login;
