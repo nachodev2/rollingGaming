5f39fe593723ae52a8e725e32a624c3811df471d
Luciana Ruiz <lucianaruiz.934@gmail.com>
2025-07-23 23:23:39 -0300

    Eliminé el llamado a componentes de prueba

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,11 @@ import Administrador from './components/pages/Administrador.jsx';
 import Error404 from './components/pages/Error404.jsx';
 import Menu from './components/shared/Menu.jsx';
 import Footer from './components/shared/Footer.jsx';
-import Tienda from './components/pages/Tienda.jsx';
 
 function App() {
 
   return (
     <>
-    <Tienda></Tienda>
       <BrowserRouter>
         <Menu></Menu>
         <main className='h-100'>
