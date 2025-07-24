ec913691c452bf0f147b22f767a6d84787059418
Claudia Ximena Barrientos <cxbarrientos1@gmail.com>
2025-07-24 00:30:56 -0300

    agregue validaciones html de los input del formulario de juegos

--- src/components/pages/producto/FormularioProducto.jsx
+++ src/components/pages/producto/FormularioProducto.jsx
@@ -8,14 +8,14 @@ const FormularioProducto = () => {
         <Row>
           <Form.Group as={Col} className="mb-3" controlId="nombreJuego">
             <Form.Label>Nombre del juego</Form.Label>
-            <Form.Control type="text" placeholder="Ej: League of Legends" />
+            <Form.Control type="text" placeholder="Ej: League of Legends" required minlength={3} maxLength={100} />
             <Form.Text className="text-danger">
               aqui ira el codigo para el error
             </Form.Text>
           </Form.Group>
           <Form.Group as={Col} className="mb-3" controlId="desarrolladorJuego">
             <Form.Label>Desarrollador</Form.Label>
-            <Form.Control type="text" placeholder="Ej: Riot Games" />
+            <Form.Control type="text" placeholder="Ej: Riot Games" required minlength={3} maxLength={100} />
             <Form.Text className="text-danger">
               aqui ira el codigo para el error
             </Form.Text>
@@ -24,7 +24,7 @@ const FormularioProducto = () => {
         <Row>
           <Form.Group as={Col} className="mb-3" controlId="categoriaJuego">
             <Form.Label>Categoria</Form.Label>
-            <Form.Select id="categoriaJuego">
+            <Form.Select id="categoriaJuego" required>
               <option>Elige una categoria</option>
               <option value="accionAventura">Accion y aventura</option>
               <option value="estrategia">Estrategia</option>
@@ -39,7 +39,7 @@ const FormularioProducto = () => {
           </Form.Group>
           <Form.Group as={Col} className="mb-3" controlId="precioJuego">
             <Form.Label>Precio</Form.Label>
-            <Form.Control type="number" placeholder="Ej: 20" step={0.01} />
+            <Form.Control type="number" placeholder="Ej: 20" step={0.01} required min={0.01} max={1000}/>
             <Form.Text className="text-danger">
               aqui ira el codigo para el error
             </Form.Text>
@@ -50,6 +50,8 @@ const FormularioProducto = () => {
           <Form.Control
             type="text"
             placeholder="Ej: https://images.pexels.com/photos/3165335/pexels-photo-3165335.jpeg"
+            required
+            minLength={10}
           />
           <Form.Text className="text-danger">
             aqui ira el codigo para el error
@@ -62,6 +64,7 @@ const FormularioProducto = () => {
             placeholder="Ej: League of Legends (LoL) es un videojuego de estrategia por equipos, donde dos equipos de cinco jugadores se enfrentan para destruir la base del otro. Los jugadores eligen entre una variedad de campeones, cada uno con habilidades únicas, y luchan en un mapa llamado 'La Grieta del Invocador'. El juego se centra en la cooperación, la estrategia y la habilidad individual para dominar el campo de batalla y lograr la victoria"
             as="textarea"
             rows={3}
+            required minlength={10} maxLength={500}
           />
           <Form.Text className="text-danger">
             aqui ira el codigo para el error
@@ -74,6 +77,7 @@ const FormularioProducto = () => {
             placeholder="Ej: En PC, los requisitos mínimos incluyen un procesador de 3 GHz, 2 GB de RAM, 12 GB de espacio libre en disco y un sistema operativo Windows 7 o superior."
             as="textarea"
             rows={2}
+            required minlength={5} maxLength={250}
           />
           <Form.Text className="text-danger">
             aqui ira el codigo para el error
