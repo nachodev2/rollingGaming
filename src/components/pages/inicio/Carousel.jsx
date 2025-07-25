b2ae8bba679e5507fe54c6d5a2a47b6915bdb7e7
Tomas Ruiz <ruiztms20@gmail.com>
2025-07-25 03:34:16 -0300

    Termine el carousel con 3 imagenes

--- src/components/pages/inicio/Carousel.jsx
+++ src/components/pages/inicio/Carousel.jsx
@@ -1,40 +1,63 @@
-import Carousel from 'react-bootstrap/Carousel';
-import './inicio.css'
+import Carousel from "react-bootstrap/Carousel";
+import "./inicio.css";
+import { Link } from "react-router";
 
-function CarouselFadeExample() {
+function CarouselPublicidadJuegos() {
   return (
     <Carousel fade>
       <Carousel.Item>
         <div className="carousel-slide-content">
           <Carousel.Caption className="carousel-caption-left">
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
-          <img src="/zlatan-fc-26.webp" alt="" />
+          <div className="carousel-image-wrapper">
+            <img src="/eafc26.png" alt="" />
+            <div className="carousel-caption-left">
+              <h2 className="display-5">EA SPORTS FC™ 26 Ultimate Edition</h2>
+              <Link to="/Error404">
+              <button className="reserve-btn">
+                RESERVAR <span>&#8250;</span>
+              </button>
+              </Link>
+            </div>
+          </div>
         </div>
       </Carousel.Item>
-      <Carousel.Item>
+       <Carousel.Item>
         <div className="carousel-slide-content">
           <Carousel.Caption className="carousel-caption-left">
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
-          <img src="/zlatan-fc-26.webp" alt="" />
+          <div className="carousel-image-wrapper">
+            <img src="/acshadows.png" alt="" />
+            <div className="carousel-caption-left">
+              <h2 className="display-5">ASSASSIN'S CREED SHADOWS</h2>
+              <Link to="/Error404">
+              <button className="reserve-btn-red">
+                COMPRAR <span>&#8250;</span>
+              </button>
+              </Link>
+            </div>
+          </div>
         </div>
       </Carousel.Item>
-      <Carousel.Item>
+       <Carousel.Item>
         <div className="carousel-slide-content">
           <Carousel.Caption className="carousel-caption-left">
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
           </Carousel.Caption>
-          <img src="/zlatan-fc-26.webp" alt="" />
+          <div className="carousel-image-wrapper">
+            <img src="/codww2.png" alt="" />
+            <div className="carousel-caption-left">
+              <h2 className="display-5">CALL OF DUTY WWII</h2>
+              <Link to="/Error404">
+              <button className="reserve-btn-yellow">
+                COMPRAR <span>&#8250;</span>
+              </button>
+              </Link>
+            </div>
+          </div>
         </div>
       </Carousel.Item>
     </Carousel>
   );
 }
 
-export default CarouselFadeExample;
+export default CarouselPublicidadJuegos;
