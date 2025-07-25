9187bb1256bafe81d20f4300e93dd6bad7b9a139
Alessandra Borges <aleborges110900@gmail.com>
2025-07-25 01:10:12 -0300

    He creado y estilizado la pagina de FavoritoVacio

--- src/components/pages/favoritos/FavoritosVacio.jsx
+++ src/components/pages/favoritos/FavoritosVacio.jsx
@@ -1,18 +1,22 @@
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import './favoritos.css';
+import { Container, Row, Col, Button } from 'react-bootstrap'; 
 
-function Favoritos() {
+function FavoritosVacio() {
   return (
-    <div>
-      <h1>Mis Favoritos</h1>
-      <p>¡Todavía no agregaste nada a favoritos!</p>
-      <div>
-        /* Aquí podrías agregar una imagen o un ícono representativo de favoritos vacíos */
-      </div>
-      <Link to="/">
-        <button>Explorar contenidos</button>
-      </Link>
-    </div>
+    <Container className="text-center my-5 ">
+        <Row>
+            <Col>
+                <i className="bi bi-heart"></i>
+                <h2 className="mb-3 text-center">¡Favoritos está vacío!</h2>
+                <p className='mb-4'>¡Agrega tus juegos favoritos!</p>
+                <Button variant="primary" href="/" className='me-5'>Ir a la tienda</Button>
+                <Button variant="primary" href="/" >Iniciar sesión</Button>
+            </Col>
+        </Row>
+    </Container>
   );
 }
 
-export default Favoritos;
+export default FavoritosVacio;
+
