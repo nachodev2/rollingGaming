1bb39e6f57eb73d9eb47e812c441680d9fa93f80
Alessandra Borges <aleborges110900@gmail.com>
2025-07-25 00:16:00 -0300

    He agregado ruta para favoritosvacio y estoy haciendo la pagina jsx

--- src/components/pages/favoritos/FavoritosVacio.jsx
+++ src/components/pages/favoritos/FavoritosVacio.jsx
@@ -1,11 +1,18 @@
 import './favoritos.css';
 
-const FavoritosVacio = () => {
+function Favoritos() {
   return (
     <div>
-      
+      <h1>Mis Favoritos</h1>
+      <p>¡Todavía no agregaste nada a favoritos!</p>
+      <div>
+        /* Aquí podrías agregar una imagen o un ícono representativo de favoritos vacíos */
+      </div>
+      <Link to="/">
+        <button>Explorar contenidos</button>
+      </Link>
     </div>
-  )
+  );
 }
 
-export default FavoritosVacio;
+export default Favoritos;
