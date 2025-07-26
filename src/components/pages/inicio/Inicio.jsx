ace5f247edb294f6f9ceb4c6ce7edb1849a9198f
Tomas Ruiz <ruiztms20@gmail.com>
2025-07-26 01:14:47 -0300

    Agregue los links y botones funcionales en el navbar

--- src/components/pages/inicio/Inicio.jsx
+++ src/components/pages/inicio/Inicio.jsx
@@ -1,4 +1,3 @@
-import { Container, Row, Col, Button, Card, Carousel } from 'react-bootstrap';
 import CarouselPublicidadJuegos from './Carousel';
 import Garantias from './Garantias';
 import GridSection from './GridSection';
