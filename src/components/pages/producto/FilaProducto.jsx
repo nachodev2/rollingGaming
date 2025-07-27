a16a2308af820fadb6d4ac6530216424d9f8c1c6
Luciana Ruiz <lucianaruiz.934@gmail.com>
2025-07-27 12:05:30 -0300

    CHice que todas las imágenes tengan el mismo tamaño y limité la cantidad de líneas de la descripción

--- src/components/pages/producto/FilaProducto.jsx
+++ src/components/pages/producto/FilaProducto.jsx
@@ -7,7 +7,7 @@ const FilaProducto = ({ juegos }) => {
     <div className="px-5">
       <Row xs={1} md={3} lg={4} className="mb-4 g-4">
         {juegosPrueba.map((juego) => (
-          <Col key={juego.id}>
+          <Col key={juego.id} className="h-100 d-flex">
             <CardProducto juego={juego} />
           </Col>
         ))}
